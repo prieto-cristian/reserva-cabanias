7141fea41f584b9f8ff7d735b8cd227e44eb2781
Cristian Prieto <pcristiandev@gmail.com>
2026-08-15 11:23:45 -0300

    Agregar la clase ReservaForm

---
+++ cabanias/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.forms import TextInput
+
+from .models import Reserva
+
+class ReservaForm(forms.ModelForm):
+    class Meta:
+        model = Reserva
+        fields = ["nombre_cliente", "email_cliente", "telefono_cliente",
+                  "fecha_ingreso", "fecha_salida"]
+        widgets = {
+            "nombre_cliente": forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            "email_cliente": forms.EmailInput(attrs={
+                'class': 'form-control',
+            }),
+            "telefono_cliente": TextInput(attrs={
+                'class': 'form-control',
+            }),
+            "fecha_ingreso": forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control',
+            }),
+            "fecha_salida": forms.DateInput(attrs={
+                'type': 'date',
+                'class': 'form-control',
+            })
+        }
