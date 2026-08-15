d8f07f4e8aff9a6b95fe81234e5d5230a7f1651e
Cristian Prieto <pcristiandev@gmail.com>
2026-08-15 11:06:23 -0300

    Definir superuser y registrar los modelos en Django admin

--- cabanias/admin.py
+++ cabanias/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Cabania, Reserva, Caracteristica
 # Register your models here.
+admin.site.register(Cabania)
+admin.site.register(Caracteristica)
+admin.site.register(Reserva)
